0a01dfc51261e1d72b5184cfcb71aad318100a5c
Przemysław Lenart <przemek.lenart@polidea.com>
2017-09-22 10:09:47 +0200

    Added basic package.json configuration.

---
+++ examples/CC2541SensorTag/src/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export default class MainComponent extends Component {
+  render() {
+    return <Text>Hello</Text>
+  }
+}
